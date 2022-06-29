27b9c006f9adc234d1ae944239265e594e5b7d78
MiguelGoda <miguel_g14@hotmail.com>
2022-06-29 19:54:24 -0400

    configuracion termianda de eslint y prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://gndx.dev', //si queremos ponemos / para que vaya al destino o esta otra forma va a un encale externo
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
